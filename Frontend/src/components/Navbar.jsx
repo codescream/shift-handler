725f510d0124d6262ea83a1f0c3f76a8021e301e
ecclesia <ogilom@gmail.com>
2024-09-13 19:51:41 -0400

    complete landing page ui

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
-
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="p-3 flex justify-between items-center bg-blue-500 h-[70px]">
+      <Link to={"/"}>
+        <img
+          src="https://res.cloudinary.com/dgbig0mad/image/upload/v1725393028/shiftly/logo.png"
+          alt="shiftly"
+          width={100}
+        />
+      </Link>
 
-export default Navbar
+      <Link to={"/login"}>
+        <Button
+          variant="outlined"
+          sx={{
+            backgroundColor: "black",
+            "&:hover": {
+              color: "white",
+            }
+          }}
+        >
+          Login
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
